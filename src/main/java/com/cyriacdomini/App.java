a7fa349283792553dd497dd0b815b738d10d710b
Cyriac Domini <cyriacd@outlook.com>
2016-12-12 11:45:20 -0500

    Maven Project init

---
+++ src/main/java/com/cyriacdomini/App.java
@@ -0,0 +1,13 @@
+package com.cyriacdomini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
