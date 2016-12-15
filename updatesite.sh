c5061b2330091f525cd155fdccc25ede636e2722
Cyriac Domini <cyriacd@outlook.com>
2016-12-14 19:22:11 -0500

    initial site subtree commit

--- updatesite.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# echo "Copying all html files..."
-# cp -r target/site/apidocs/* .
-# echo "Adding html files for staging."
-# git add *.html -f
-# git add com -f
-#
-cp target/cs*.jar .
