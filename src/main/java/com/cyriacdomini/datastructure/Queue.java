e5f7c52f0c0aec115944d941ddb8fc9949f147a7
Cyriac Domini <me@cyriacdomini.com>
2016-12-19 08:20:07 -0500

    set.java checkstyle errors fixed

--- src/main/java/com/cyriacdomini/datastructure/Queue.java
+++ src/main/java/com/cyriacdomini/datastructure/Queue.java
@@ -1,24 +1,25 @@
 package com.cyriacdomini.datastructure;
 /**
  * Queue Interface for First In First Out data structures.
+ * @param <T> Genric type T.
  */
-public interface Queue<T> extends Iterable<T>{
+public interface Queue<T> extends Iterable<T> {
   /**
-   * Adds data to end of Queue
-   * @param  data          value to enqueue to the end of the Queue
-   * @return   true is successfully enqueued
+   * Adds data to end of Queue.
+   * @param  data          value to enqueue to the end of the Queue.
+   * @return   true is successfully enqueued.
    */
-  public boolean enqueue(T data);
+  boolean enqueue(T data);
 
   /**
-   * Removes item from the head of the queue and returns it
+   * Removes item from the head of the queue and returns it.
    * @return value of data at the head of the queue
    */
-  public T dequeue();
+   T dequeue();
 
   /**
-   * Returns the size of the queue
+   * Returns the size of the queue.
    * @return the length of the queue
    */
-  public int size();
+  int size();
 }
